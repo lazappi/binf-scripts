05d22fbf3fba304a28c720fa61364a1d8c47c897
Luke Zappia <lazappi@users.noreply.github.com>
2016-06-03 16:55:08 +1000

    Save kallisto log to file

--- kallistoMulti.py
+++ kallistoMulti.py
@@ -40,15 +40,20 @@ def get_args():
     return args, kal_opts
 
 
-def setup_logging(log_file):
+def setup_logging(output_dir):
     """
     Setup logging system.
 
     Log is written to 'alignmentStats.log'.
     """
 
-    logger = logging.getLogger("kalMulti")
+    logger = logging.getLogger("kalMult")
     logger.setLevel(logging.DEBUG)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    log_file = os.path.join(output_dir, "kallistoMulti.log")
 
     # create file handler which logs even debug messages
     file_handler = logging.FileHandler(log_file)
@@ -78,32 +83,46 @@ def main():
 
     args, kal_opts = get_args()
 
-    log_file = os.path.join(args.output_dir, "kallistoMulti.log")
-
-    setup_logging(log_file)
+    setup_logging(args.output_dir)
 
     logger = logging.getLogger("kalMult." + __name__)
 
-    #input_pairs = pair_inputs(args.inputs, args.regex1, args.regex2)
     input_pairs = zip(args.read1, args.read2)
 
     failed = 0
     sample_count = 0
 
-    logger.info("Beginning kallisto quantification on multiple samples...\n")
+    logger.info("Beginning kallisto quantification on multiple samples...")
     for pair in input_pairs:
         sample = os.path.basename(pair[0])
         sample = sample.split(".")[0]
         sample_count += 1
-        logger.info("Quantifying sample" + sample_count + "of" + len(input_pairs) + "-" + sample)
+        logger.info("Quantifying sample " + sample + " (" + str(sample_count) + " of " +
+                str(len(input_pairs)) + ")...")
+
         cmd_str = "kallisto quant --index " + args.index + " --output-dir "
         cmd_str += args.output_dir + "/" + sample + " " + kal_opts + " "
         cmd_str += pair[0] + " " + pair[1]
         cmd = shlex.split(cmd_str)
-        failed = subprocess.call(cmd)
-        if failed:
-            sys.exit("kallisto command '" + cmd_str + "' failed to run")
-        logger.info("Sample" + sample + "complete\n")
+
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        kal_out = ""
+        while True:
+            out = process.stderr.read(1)
+            if out == '' and process.poll() != None:
+                break
+            if out != '':
+                kal_out += out
+                sys.stdout.write(out)
+                sys.stdout.flush()
+
+        kal_log_path = os.path.join(args.output_dir, sample, "kallisto.log")
+        with open(kal_log_path, "w") as kal_log:
+            logger.info("Writing kallisto log to " + kal_log_path + "...")
+            kal_log.write(kal_out)
+
+        logger.info("Sample " + sample + " complete")
     logger.info("Done!")
 
 
