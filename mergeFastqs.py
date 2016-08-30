3e348bd44125cd57e891267c3ec8ed393f42ac2e
Luke Zappia <lazappi@users.noreply.github.com>
2016-08-31 09:58:18 +1000

    Fix pattern argument help

--- mergeFastqs.py
+++ mergeFastqs.py
@@ -15,20 +15,21 @@ def get_args():
     parser.add_argument("-s", "--separator",
                         help="Filename separator. Default is '_'.",
                         default="_")
-    parser.add_argument("-p", "--pattern",
-                        help="""Pattern used to decide which files to merge.
-                                Should have the format [CODE][SEP]...[CODE]
-                                where SEP is the separator and CODE is one of:
-                                K = Keep this section
-                                M = Merge using this section
-                                R = Remove this section
-                                For example if the filname structure was:
-                                'SAMPLE_READ_LANE_DATE'
-                                to merge on LANE and remove DATE the pattern
-                                would be 'K_K_M_R'""")
     parser.add_argument("-o", "--outdir",
-                        help="Path to output directory for merged files",
+                        help="Path to output directory for merged files.",
                         default=os.getcwd())
+    parser.add_argument("-p", "--pattern",
+                        help="Pattern used to decide which files to merge." +
+                             "Should have the format [CODE][SEP]...[CODE] " +
+                             "where SEP is the separator and CODE is one of: " +
+                             "K = Keep this section, " +
+                             "M = Merge using this section, " +
+                             "R = Remove this section. " +
+                             "For example if the filname structure was: " +
+                             "'SAMPLE_READ_LANE_DATE', " +
+                             "to merge on LANE and remove DATE the pattern " +
+                             "would be 'K_K_M_R'.",
+                        required=True)
     parser.add_argument("fastqs",
                         metavar="FASTQ",
                         nargs="+",
